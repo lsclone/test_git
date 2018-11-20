11a3b077dcd7abc3c7148a17c83868b2a4d3c491
lsclone <lsclone@outlook.com>
2018-11-20 17:01:47 +0800

    add 2.cpp

---
+++ 2.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+using namespace std;
+
+int main()
+{
+	char* p = new char[64];
+	memset(p, 0x00, sizeof(p));
+	sprintf(p, "this is a test demo.\n");
+	printf("%s\n", p);
+	return 0;
+}
